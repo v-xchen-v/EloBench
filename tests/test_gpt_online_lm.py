d1bfe80ffc9b69c5430ae4db1bef874ea9b2dbed
v-xchen-v <407081305@qq.com>
2023-12-28 02:58:47 +0000

    add test of online llm gen ans

--- tests/test_gpt_online_lm.py
+++ tests/test_gpt_online_lm.py
@@ -8,3 +8,27 @@ class TestGPTOnlineLM(unittest.TestCase):
         answer = model.generate_answer(question)
         self.assertIsInstance(answer, str)
         self.assertNotEqual(answer, "")
+        
+    def test_batch_generate_answer(self):
+        model = GPTOnlineLM("gpt-35-turbo")
+        questions = [ \
+            "Does light travel forever or does it eventually fade?",
+            "What is France's record in wars?",
+            "Do you think Rihanna's image has influenced the fashion industry, and if so, how?",
+            "Why does the United States have higher living standards than Europe?",
+            'Can you name the main actors in the film "Green Book"?',
+            "What are some crazy coincidences in history ?",
+            "Is anything in England actually original? Tea, for example, comes from China. Fish & chips from Portugal. Football (soccer) originates in China. Gothic style architecture is French.",
+            "Have you ever tried adding exotic fruits to your overnight oats?",
+            "Can you name a few controversies that have involved Jeremy Clarkson?",
+            "Can you tell me about the impact of the iPad 3 on the consumer electronics industry?",
+            "Who are some actors that have worked with Kevin Hart in his comedy films?",
+            "How does an Andrea Bocelli concert compare to other concerts you've been to?",
+            "How has Katy Perry's image evolved over the years?",
+            "Hi, I'm interested in learning to play badminton. Can you explain the game to me?",
+        ]
+        answers = model.batch_generate_answer(questions)
+        for idx, answer in enumerate(answers):
+            print(f'Question: {questions[idx]}\nAnswer: {answer}\n\n')
+            self.assertIsInstance(answer, str)
+            self.assertNotEqual(answer, "")
