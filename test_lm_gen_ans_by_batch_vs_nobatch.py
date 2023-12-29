041e6685833af601b64f2db283014173f048b7d1
v-xchen-v <407081305@qq.com>
2023-12-29 02:48:59 +0000

    update tested model

--- test_lm_gen_ans_by_batch_vs_nobatch.py
+++ test_lm_gen_ans_by_batch_vs_nobatch.py
@@ -1,16 +1,16 @@
 from models.hf_local_lm import AutoCausalLM
 
-
-        
 # model = AutoCausalLM("meta-llama/Llama-2-7b-chat-hf") # √
 # model = AutoCausalLM("mosaicml/mpt-7b-chat") # x
 # model = AutoCausalLM("tiiuae/falcon-7b-instruct") # x
 # model = AutoCausalLM("msys/vicuna-7b-v1.5") # √
 # model = AutoCausalLM("chavinlo/alpaca-native") # √
 # model = AutoCausalLM("mosaicml/mpt-30b-chat") # x
-model = AutoCausalLM("WizardLM/WizardLM-7B-V1.0") # √
+# model = AutoCausalLM("WizardLM/WizardLM-7B-V1.0") # √
 # model = AutoCausalLM("WizardLM/WizardLM-13B-V1.2") # √
 # model = AutoCausalLM("Xwin-LM/Xwin-LM-7B-V0.1") # √
+# model = AutoCausalLM("chavinlo/alpaca-13b") # √
+
 questions = [ \
     "Does light travel forever or does it eventually fade?",
     "What is France's record in wars?",
@@ -28,8 +28,13 @@ questions = [ \
     "Hi, I'm interested in learning to play badminton. Can you explain the game to me?",
 ]
 model.batch_size = len(questions)
-answer = model.generate_answer(questions[0], free_model_when_exit=False)
-answers = [x for x in model.batch_generate_answer(questions, free_model_when_exit=False)]
-print(answer)
-print(answers[0])
-print(answer == answers[0])
+single_generate_answers = []
+for question in questions:
+    single_generate_answers.append(model.generate_answer(question, free_model_when_exit=False))
+# answer = model.generate_answer(questions[0], free_model_when_exit=False)
+batch_generation_answers = [x for x in model.batch_generate_answer(questions, free_model_when_exit=False)]
+# print(answer)
+# print(batch_generation_answers[0])
+for i in range(len(questions)):
+    print(single_generate_answers[i] == batch_generation_answers[i])
+# print(single_generate_answers == batch_generation_answers)
